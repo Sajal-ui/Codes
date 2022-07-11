2a731e930c3633b990fcfb2493dcbf9af68ec3c8
Sajal <19ucc031@lnmiit.ac.in>
2022-07-12 03:09:47 +0530

    New

--- CodingBlocks/Coding/src/Rectangle_Cutting.java
+++ CodingBlocks/Coding/src/Rectangle_Cutting.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Main {
+public class Rectangle_Cutting {
     public static void main(String[] args) throws IOException{
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
